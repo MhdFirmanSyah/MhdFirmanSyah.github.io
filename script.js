e012dbb8c711071681a169ec89c9fe710b1b2aa8
FirmanSyah748 <mhdfirmansyah748@gmail.com>
2024-03-13 18:01:51 +0700

    update

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ window.onscroll = function(){
         document.querySelector("nav").style.top = "0";
     }
 }
+
+// scroll lock
+document.documentElement.style.overflowX = 'hidden';
+document.documentElement.style.overflowY = 'hidden';
+function toggleScrool(enable){
+    if (enable) {
+        document.documentElement.style.overflowY = 'auto';
+    }
+    else {
+        document.documentElement.style.overflowY = 'hidden';
+    }
+}
+m 
